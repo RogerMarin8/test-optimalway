fa729e9d242d69acddf24160955dea4fd48307ae
RogerMarin8 <roger.marin@estudiants.urv.cat>
2024-09-26 15:26:28 +0200

    Repository & DB connection

---
+++ src/main/java/com/example/test_optimalway/TestOptimalwayApplication.java
@@ -0,0 +1,13 @@
+package com.example.test_optimalway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestOptimalwayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestOptimalwayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/test_optimalway/TestOptimalwayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.test_optimalway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestOptimalwayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
